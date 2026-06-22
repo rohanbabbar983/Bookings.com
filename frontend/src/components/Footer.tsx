beebfb7f68f7c26e6b4b3a7fd13036d5f1975978
Rohan Babbar <rohanbabbar2003@gmail.com>
2026-06-22 23:15:21 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa6";
+// import { FaGithub, FaLinkedin } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
